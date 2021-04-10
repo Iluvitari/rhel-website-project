36146726b7456f441eab8acd38cd5950cad479b5
Iluvitari <48405834+Iluvitari@users.noreply.github.com>
2021-04-10 16:20:03 +0100

    removed section from website

--- src/index.php
+++ src/index.php
@@ -4,14 +4,6 @@
     </head>
  
     <body>
-        #<header>
-        #<ul>
-        #<li>Interview Website</li>
-        #<li>Testing Code Update</li>
-        #<li>unautomated</li>
-        #</ul>
-        #</header>
-
     <?php
         $dbhost = '192.168.4.103:3306';
         $dbuser = 'dbuser';
