2ee4dd1999975fca78c1ce8c69d1a704789bef74
Iluvitari <48405834+Iluvitari@users.noreply.github.com>
2021-04-13 14:44:30 +0100

    test update

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
         <ul>
         <li>Interview Website</li>
         <li>Testing Code Update</li>
-        <li>AUTOMATED TEST attempt 2</li>
+        <li>test update</li>
         </ul>
         </header>
 
